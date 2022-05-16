0019fb4cafb11c272da03a1038bab952e557a066
Jesús Jiménez <60352923+jesus-antulio@users.noreply.github.com>
2022-05-16 10:19:34 -0500

    update estilos

--- pages/login.php
+++ pages/login.php
@@ -8,14 +8,14 @@
     <link rel="shortcut icon" type="image/jpg" href="../assets/logo.png" />
     <script src="https://kit.fontawesome.com/933e55e7ca.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/compartido.css">
-    <link rel="stylesheet" href="../styles/login.css">
+    <link rel="stylesheet" href="../styles/login-singup.css">
 </head>
 <body>
     <?php include '../components/header.php'; ?>
     <div class="container">
         <h1> Iniciar Sesión </h1>
         <div class="flex-container">
-            <div class="login">
+            <div class="datos">
                 <form action="../controllers/login.php" method="post">
                     <p> Correo Electrónico </p>
                     <input type="text" name="correo" placeholder="Correo">
@@ -26,9 +26,9 @@
                 </form>
             </div>
             <div class="line"></div>
-            <div class="registro">
+            <div class="opciones">
                 <h2> ¿No tienes una cuenta? </h2>
-                <a href="registro.php">Registrate</a>
+                <a href="singup.php">Registrate</a>
             </div>
         </div>
     </div>
