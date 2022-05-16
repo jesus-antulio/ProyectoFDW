f1c4e14a1a8e95418066b6ad5c1001d58d22f1fd
Jesús Jiménez <60352923+jesus-antulio@users.noreply.github.com>
2022-05-16 17:54:59 -0500

    Acceso a BD

---
+++ database/CAD.php
@@ -0,0 +1,9 @@
+<?php
+#CAD -> Capa de Acceso de Datos
+    require_once 'conexion.php';
+
+    class CAD{
+        public $con;
+
+    }
+?>
---
+++ database/conexion.php
@@ -0,0 +1,28 @@
+<?php
+    #Clase para realizar la conexion a la base de datos
+    class Conexion {
+        #Atributos
+        private $host; //Localhots o IP
+        private $db; //nombre de la base de datos
+        private $usuario; //Usuario de la base de datos
+        private $pass; //Contraseña del usuario de la base de datos
+        private $charset; //Codificación de la base de datos (UTF8)
+
+        #Constructor
+        public function __construct(){
+            $this->host = "localhost";
+            $this->db = "usuarios";
+            $this->usuario = "root";
+            $this->pass = "";
+            $this->charset = "utf8";
+        }
+
+        #Metodo para conectar a la base de datos
+        function conectar() {
+            #Conectar a la BD --> PDO
+            $com = "mysql:host=".$this->host.";dbname=".$this->db.";charset=".$this->charset;
+            $enlace = new PDO($com, $this->usuario, $this->pass);   //Instancia de la clase PDO
+            return $enlace;
+        }
+    }
+?>
