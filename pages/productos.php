830f848625c70d56f45d4bf0a8ca304ea76dd7b2
Jesús Jiménez <60352923+jesus-antulio@users.noreply.github.com>
2022-05-16 17:54:44 -0500

    Maquetación de "Productos.php"

--- pages/productos.php
+++ pages/productos.php
@@ -1,18 +1,40 @@
-<html>
-    <head>
-        <title>Reposteria | Productos</title>
-        <link rel="shortcut icon" type="image/jpg" href="../assets/logo.png" />
-        <script src="https://kit.fontawesome.com/933e55e7ca.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="../styles/compartido.css">
-        <link rel="stylesheet" href="../styles/productos.css">
-    </head>
-    <body>
-        <?php
-            include '../components/header.php';
-        ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposteria | Productos </title>
+    <link rel="shortcut icon" type="image/jpg" href="../assets/logo.png" />
+    <script src="https://kit.fontawesome.com/933e55e7ca.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/product.css">
+    <link rel="stylesheet" href="../styles/compartido.css">
+</head>
+<body>
+    <?php include '../components/header.php'; ?>
 
-        <?php
-            include '../components/footer.php';
-        ?>
-    </body>
+    <div class="banner"></div>
+
+    <div class="container">
+        <h1>Productos</h1>
+        <div class="flex-container">
+            <?php
+                echo "
+                <div class='producto'>
+                    <div class='producto-img'>
+                        img
+                    </div>
+                    <div class='producto-info'>
+                        <h2>Producto 1</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                        <a href='#'>Ver más</a>
+                        <label>$$$</label>
+                    </div>
+                </div>";
+            ?>
+        </div>
+    </div>
+
+    <?php include '../components/footer.php'; ?>
+</body>
 </html>
