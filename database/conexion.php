67fd960a62114b4819193ab1fd7622d77e909182
Jesús Jiménez <60352923+jesus-antulio@users.noreply.github.com>
2022-05-23 10:02:07 -0500

    update

--- database/conexion.php
+++ database/conexion.php
@@ -10,7 +10,7 @@
 
         #Constructor
         public function __construct(){
-            $this->host = "localhost:5000";
+            $this->host = "localhost:3307";
             $this->db = "reposteria";
             $this->usuario = "root";
             $this->pass = "";
