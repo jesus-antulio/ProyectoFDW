9a5b189dd00288acc408e1b081d332c311d8f837
Jesús Jiménez <60352923+jesus-antulio@users.noreply.github.com>
2022-05-24 09:17:56 -0500

    Se agrego la pantalla de "infp-producto.php"

--- pages/info-producto.php
+++ pages/info-producto.php
@@ -5,26 +5,16 @@
 <head>
     <?php include '../components/info.php'; ?>
     <title>Resposteria | Productos </title>
-    <link rel="stylesheet" href="../styles/product.css">
+    <link rel="stylesheet" href="../styles/product-info.css">
 </head>
 <body>
     <?php include '../components/header.php'; ?>
 
-    <div class="banner"></div>
-
     <div class="container">
-        <h1>Productos</h1>
         <?php
-        if (isset($_SESSION['rol']) && $_SESSION['rol'] == 2) {
-            echo '
-            <center>
-                <a href="../pages/agregarProducto.php" class="btn-agregar">Agregar Productos</a>
-            </center>';
-        }
+        echo "<h1>Productos</h1>";
         ?>
-        <div class="flex-container">
-            
-        </div>
+
     </div>
 
     <?php include '../components/footer.php'; ?>
